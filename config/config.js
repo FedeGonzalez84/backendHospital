090cfb8ccdf59931ae0e35d847c32927d70a97cc
fedegonzalez <fedegonzalez_84@hotmail.com>
2018-09-15 09:02:04 -0300

    Backend finalizado

---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '984659642413-apaqq3oo5vrmas5osgg347i2cb6c466f.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'NHkGgLJn-B7J1fEUb9NIZr8i';
